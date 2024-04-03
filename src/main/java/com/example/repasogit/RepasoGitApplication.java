9785e0a1bb5d1900caeed5afe6036b2d0fe141ba
SergioABS0813 <sergioandre.bustamante@gmail.com>
2024-04-03 17:34:04 -0500

    Project
    subiendo el proyecto

---
+++ src/main/java/com/example/repasogit/RepasoGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.repasogit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepasoGitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/repasogit/RepasoGitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.repasogit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoGitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
